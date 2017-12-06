9c213d821afc5ce08d521a622689c3afd3ad3e11
peterianmuller <peterianmuller@gmail.com>
2017-12-05 19:01:49 -0800

    Begin dequeue method on QueueES5

--- data-structures/es5/queue.js
+++ data-structures/es5/queue.js
@@ -6,13 +6,24 @@ function QueueES5(){
 
 QueueES5.prototype.enqueue = function(data){
 	// add something to empty queue
-	if (!this.oldestIndex) {
+	if (this.oldestIndex === null) {
   	this.oldestIndex = 0;
   	this.newestIndex = 0;
-  	this.storage[0] = data;
+  	this.storage[this.newestIndex] = data;
   	return data;		
 	}
   this.newestIndex++;
   this.storage[this.newestIndex] = data;
   return data;
+}
+
+QueueES5.prototype.dequeue = function(){
+	// if empty queue return null
+	if (this.oldestIndex === null && this.newestIndex === null) {
+  	return null; 
+	}
+	// what if we don't delete from storage but instead move our pointers
+	var oldestItem = this.storage[this.oldestIndex];
+	this.oldestIndex++;
+	return oldestIndex;
 }
