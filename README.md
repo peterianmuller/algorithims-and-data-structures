bfc9f3289fb8715c795e022dd46a1d83d77dde5b
peterianmuller <peterianmuller@gmail.com>
2018-02-03 12:14:01 -0800

    Add implamentation of breath first and depth first traversal to es6/bst.js

--- README.md
+++ README.md
@@ -9,4 +9,25 @@ Run npm install to inport mocha and chai as dependencies
 
 Binary Search Trees
 - Because binary seach trees consist of nested nodes, we tend to use recursion to traverse the nodes that make up the tree.
--BST's are binary trees that have the requirement that each left child node must be less than the parent node and each right child node must be greater than the parent node
+- BST's are binary trees that have the requirement that each left child node must be less than the parent node and each right child node must be greater than the parent node
+
+- breath first traversal (bft) vs depth first traversal (dft)
+
+bft uses a queue in order to keep track of the nodes that we want to visit. We add the root node to the queue, and then any nodes to the root's left or right properties.
+
+Pseudocode for bft
+
+declare var queue pointing to an array with the this value
+while queue has a length
+	use shift to grab element at index 0 in queue and save reference to it as node
+	invoke call back on that element
+	check if node.left exists
+		if so add it to the back of the queue
+	check if node.right exists
+	  if so add it to the back of the queue	
+
+Note that we don't need to recurse. The while loop will continue to add nodes further down the tree via the checks for node.left and node.right. Also the focal node changes for each iteration, from line 22	  
+
+
+
+
--- data-structures/es6/bst.js
+++ data-structures/es6/bst.js
@@ -3,7 +3,9 @@
   // each node has a left and right property that also can point to nodes
   // each parent node's value property must be greater than it's left node's value property and less than it's right node's value property
 
-  //insertion O(log n)
+  // insertion O(log n)
+  // contains O(log n)
+    // this means that we can eliminate the possibilities by 1/2 each time we recurse. We can decide at the beginning of our insertion or contains if we'll visit the left-hand side of the BST or the left-hand side of the BST
 
 
   // insert pseudocode 
@@ -17,7 +19,57 @@
         // if true invoke this.right.insert and pass in value
         // else assign this.right to new node		   
     
+// contains
 
+// check if value equals current node's value property
+  // if true return true
+// check if value is less than current node's value property
+  // if true check if current node has a left property
+    // if true invoke contains on node at left property
+    // if false return false
+  // if false check if current node has a right property
+    // if turue invoke contains on node at right property
+    // if false invoke contains on nade at left property
+
+
+ // traversing tree
+ 
+ // depth-first-traversal -> you start at the root node and traverse a branch all the way down to the bottom most node or leaf node. 
+
+ // I -> callback
+ // O -> value of callback on each node
+ // C -> must visit bottom most node before the sibling node
+ // E  
+
+ // dfs
+ // check if this.left exists
+   // if true invoke dfs on this.left
+ // invoke callback on current node (this)
+ // check if this.right exists
+   // if true invoke dfs on this.right
+
+ // breath-first search
+ // referred to as bfs
+
+ // I -> callback
+ // O -> value of callback invoked on each node
+ // C -> must visit each sibling node before reaching leaves
+ // E
+
+ // check if this.node exists
+   // if false return
+ // declare queue variable pointing to []
+ // add this.left, and this.right to queue
+ // while queue has a length greater than 0 
+ 	// unshift first element (node) from queue
+ 	  // invoke callback on node
+ // invoke bfs on this.left	  
+
+
+  
+
+
+ 
 
 
 
@@ -44,7 +96,69 @@ class BinarySearchTreeNode {
 			}
 		}
 	}
+
+	contains(value){
+		if (value === this.value) {
+			return true;
+		} else if (value < this.value) {
+			if (this.left) {
+				return this.left.contains(value);
+			} else {
+				return false;
+			}
+		} else {
+			if (this.right) {
+        return this.right.contains(value);
+			} else {
+				return false;
+			}
+		}
+	}
+
+	dft(callback){
+		if (this.left) {
+			this.left.dfs(callback)
+		} 
+		callback(this)
+		if (this.right) {
+			this.right.dfs(callback);
+		} 
+	}
+
+	bft(cb) {
+	  let queue = [this];
+	  while(queue.length) {
+	  	let node = queue.shift();
+      cb(node)
+      if (node.left) {
+      	queue.push(node.left)
+      }
+      if (node.right) {
+      	queue.push(node.right);
+      }
+	  }
+	}
+
+
+
 }
+
+let peteTree = new BinarySearchTreeNode(25);
+peteTree.insert(20);
+peteTree.insert(36);
+peteTree.insert(10);
+peteTree.insert(22);
+peteTree.insert(30);
+peteTree.insert(40);
+
+log = (node) => {
+	console.log(node.value);
+}
+
+//peteTree.dft(log);
+peteTree.bft(log);
+
+//console.log([1,2,3].shift());
 
 
 
